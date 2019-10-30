77c2301be0ea21cd6f7d712201e12b2d9ae57d43
yinzhida <yinzhida@qiyi.com>
2019-10-30 21:40:53 +0800

    deving

--- src/js/clazz/srt-engine.js
+++ src/js/clazz/srt-engine.js
@@ -1,18 +1,114 @@
 import getLogger from '@/js/utils/logger';
-// import
+import { formatTime } from '@/js/utils/time';
+import newGUID from '@/js/utils/guid';
+
+import { getSubtitleTextArrayByUrl, getTextArrayFromText } from '@/js/utils/srt';
 
 class SrtEngine {
-  constructor () {
-    this.content = null;
+  constructor (shouldBuildIndex) {
     this.logger = getLogger('SrtEngine');
-    this.url = '';
-    this.dataJSON = null;
+    this.content = [];
+    this.url = null;
+    this.shouldBuildIndex = typeof shouldBuildIndex === 'boolean' ? shouldBuildIndex : true;
+    this.indexGroup = null;
   }
 
   load (url) {
+    this.url = url;
+    this.content = getSubtitleTextArrayByUrl(url);
+    if (this.shouldBuildIndex) {
+      this.buildIndex();
+    }
   }
 
-  compile (content) {
+  compile (text) {
+    this.url = '';
+    this.content = getTextArrayFromText(text);
+    if (this.shouldBuildIndex) {
+      this.buildIndex();
+    }
+  }
+
+  stringify () {
+    let result = '';
+    let len = this.content.length;
+    for (let i = 0; i < len; i++) {
+      let data = this.content[i];
+      let id = i + 1;
+      result += id + '\n';
+      result += formatTime((data.startTimeInMilliSeconds / 1000), 'hh:mm:ss,S');
+      result += ' --> ';
+      result += formatTime((data.endTimeInMilliSeconds / 1000), 'hh:mm:ss,S');
+      result += '\n';
+      result += data.texts.join('\n') + '\n\n';
+    }
+    return result;
+  }
+
+  buildIndex () {
+    const len = this.content.length;
+    this.indexGroup = {};
+    for (let i = 0; i < len; i++) {
+      const data = this.content[i];
+      const levelKey = this.getLevelKey(data.startTimeInMilliSeconds);
+      this.indexGroup[levelKey] = this.indexGroup[levelKey] || [];
+      this.indexGroup[levelKey].push(data);
+    }
+  }
+
+  findByTime (milliSecondtime) {
+    if (this.indexGroup) { // 通过索引查询
+      const levelKey = this.getLevelKey(milliSecondtime);
+      return this.indexGroup[levelKey].find((item) => {
+        return item.startTimeInMilliSeconds < milliSecondtime &&
+          item.endTimeInMilliSeconds > milliSecondtime;
+      });
+    } else { // 直接轮询查询
+      let len = this.content.length;
+      for (let i = 0; i < len; i++) {
+        let data = this.content[i];
+        if (data.startTimeInMilliSeconds < milliSecondtime &&
+          data.endTimeInMilliSeconds > milliSecondtime) {
+          return data;
+        }
+      }
+    }
+  }
+
+  getLevelKey (milliSecondtime) {
+    return parseInt(milliSecondtime / 100 / 60 / 10);
+  }
+
+  findByText () {
+
+  }
+
+  findById () {
+
+  }
+
+  addDialogue ({ startTimeInMilliSeconds, endTimeInMilliSeconds, texts, index }) {
+    let data = {
+      uid: newGUID(),
+      id: index,
+      startTimeInMilliSeconds,
+      endTimeInMilliSeconds,
+      texts,
+    };
+    this.content.splice(index, 0, data);
+    this.updateIndex();
+  }
+
+  updateDialogue () {
+
+  }
+
+  removeDialogue () {
+
+  }
+
+  getContent () {
+    return this.content;
   }
 }
 
--- src/js/utils/srt.js
+++ src/js/utils/srt.js
@@ -2,7 +2,7 @@
  * Created Date: 2018-04-18 5:34:06
  * Author: yinzhida Email: zhaoxinxin@qiyi.com
  * -----
- * Last Modified: 2019-10-30 17:52:59
+ * Last Modified: 2019-10-30 21:35:43
  * Modified By: yinzhida yinzhida@qiyi.com
  * -----
  * Copyright (c) 2018 IQIYI
@@ -10,9 +10,10 @@
 
 import { fetchBlob } from '@/dao/dao';
 import DataCache from './data-cache';
+import newGUID from 'guid';
 
 const getMilliSecondsFromString = (timeStr) => {
-  //时间格式转换
+  // 时间格式转换
   let timeDivide = timeStr.split(',');
   let milliSecond = Number(timeDivide[1]);
   let otherTimeStr = timeDivide[0];
@@ -21,70 +22,23 @@ const getMilliSecondsFromString = (timeStr) => {
   return milliSecond;
 };
 
-const compileTextLine = (text) => {
+const getPureText = (text) => {
   if (!text) {
-    return {
-      text: '',
-      styles: {},
-    }
+    return '';
   }
-
-  let fontSize, fontColor, bold, italic, underline, align;
-  const alignMatch = text.match(/\{\\(an\d)\}/);
-  if (alignMatch) {
-    align = alignMatch[1];
-  }
-
-  const fontSizeMatch = text.match(/<font\s+[^>]*?size="?(\d+)"?/);
-  if (fontSizeMatch) {
-    fontSize = parseInt(fontSizeMatch[1]);
-  }
-
-  const fontColorMatch = text.match(/<font\s+[^>]*?color="?(\d+)"?/);
-  if (fontColorMatch) {
-    fontColor = fontSizeMatch[1];
-  }
-
-  const boldMatch = text.match(/(<b>|<B>|<bold>|<BOLD>)/);
-  if (boldMatch) {
-    bold = true;
-  }
-
-  const italicMatch = text.match(/(<i>|<I>)/);
-  if (italicMatch) {
-    italic = true;
-  }
-
-  const underlineMatch = text.match(/(<u>|<U>)/);
-  if (underlineMatch) {
-    underline = true;
-  }
-
-  let showText = text.replace(/{\\.*?}/g, '').replace(/<\/?(font|b|B|bold|BOLD|i|I|u|U).*?>/g, '');
-
-  return {
-    text: showText,
-    styles: {
-      fontSize,
-      fontColor,
-      bold,
-      italic,
-      underline,
-      align,
-    }
-  }
-}
+  return text.replace(/{\\.*?}/g, '').replace(/<\/?.*?>/g, '');
+};
 
 const getTextArrayFromText = (text) => {
   let lineArray = text.split('\n');
   try {
-    //一条字幕的开始点
+    // 一条字幕的开始点
     let marker = 0;
     let dataArray = [];
     for (let i = 0; i < lineArray.length; i++) {
-      //here is a start point of data
+      // here is a start point of data
       if (i === marker) {
-        //default timeString
+        // default timeString
         let timeArr = ['00:00:00,000', '00:00:00,000'];
         if ((i + 1) >= lineArray.length) {
           return dataArray;
@@ -92,28 +46,25 @@ const getTextArrayFromText = (text) => {
 
         let timeStr = lineArray[i + 1];
         if (timeStr !== '') {
-          timeArr = timeStr.trim().split('-->');
+          timeArr = timeStr.trim().split(/\s+/).filter((item) => {
+            return /\d{1,2}:\d{1,2}:\d{1,2},\d{1,3}/.test(item);
+          });
         }
 
         let data = {
-          id: lineArray[i].trim(), //id
-          idOriginal: lineArray[i], //id
-          timeStr: timeStr, //timeStr
-          startTimeStr: timeArr[0], //starttime
-          endTimeStr: timeArr[1], //endtime
-          texts: [] //subtitle content,may have several lines
+          uid: newGUID(),
+          id: lineArray[i].trim(), // id
+          startTimeInMilliSeconds: getMilliSecondsFromString(timeArr[0]), // 根据字幕里的事件字符串，获取开始结束的以毫秒计数的时间点
+          endTimeInMilliSeconds: getMilliSecondsFromString(timeArr[1]), // 根据字幕里的事件字符串，获取开始结束的以毫秒计数的时间点
+          texts: [] // subtitle content,may have several lines
         };
 
-        //根据字幕里的事件字符串，获取开始结束的以毫秒计数的时间点
-        data.startTimeInMilliSeconds = getMilliSecondsFromString(data.startTimeStr);
-        data.endTimeInMilliSeconds = getMilliSecondsFromString(data.endTimeStr);
-
-        //如果这些数据有误，说明这个字幕文件格式不太对。
+        // 如果这些数据有误，说明这个字幕文件格式不太对。
         if (isNaN(data.id) || isNaN(data.startTimeInMilliSeconds) || isNaN(data.endTimeInMilliSeconds)) {
           throw new Error('字幕文件的内容，格式有误！');
         }
 
-        //now point to the content line i+2，就是说id和时间后边，是这个字幕的内容，这部分是从第三行开始
+        // now point to the content line i+2，就是说id和时间后边，是这个字幕的内容，这部分是从第三行开始
         let contentLineNumber = 2;
         if ((i + contentLineNumber) >= lineArray.length) {
           data.texts.push('');
@@ -121,8 +72,8 @@ const getTextArrayFromText = (text) => {
           return dataArray;
         }
 
-        //当然这个字幕到底是有一行字幕还是有两行或者三行字幕都不好说，所以做一下循环，看看到底有几行
-        //if line i+2 have words, then it must be the subtitle contentline. and we should find it's next line, util we get a '', another case is there is'nt content line.
+        // 当然这个字幕到底是有一行字幕还是有两行或者三行字幕都不好说，所以做一下循环，看看到底有几行
+        // if line i+2 have words, then it must be the subtitle contentline. and we should find it's next line, util we get a '', another case is there is'nt content line.
         while (lineArray[i + contentLineNumber].trim() !== '') {
           data.texts.push(lineArray[i + contentLineNumber]);
           contentLineNumber++;
@@ -133,8 +84,8 @@ const getTextArrayFromText = (text) => {
           }
         }
 
-        //查找完字幕内容，就该来分析一下，这句字幕是不是结束啦，后边空行是不是有多于一行，然后计算出下一句字幕的开始位置（marker）。
-        //now we know at the index 'i+contentLineNumber' we get a '', so we should see the next line 'i+contentLineNumber+1' until we get some words.
+        // 查找完字幕内容，就该来分析一下，这句字幕是不是结束啦，后边空行是不是有多于一行，然后计算出下一句字幕的开始位置（marker）。
+        // now we know at the index 'i+contentLineNumber' we get a '', so we should see the next line 'i+contentLineNumber+1' until we get some words.
         let divideLineNumber = i + contentLineNumber + 1;
         if (divideLineNumber >= lineArray.length) {
           marker = 0;
@@ -142,7 +93,7 @@ const getTextArrayFromText = (text) => {
           return dataArray;
         }
 
-        //这个地方空行可能也不止一行，不过按理说应该是一行才对。
+        // 这个地方空行可能也不止一行，不过按理说应该是一行才对。
         while (lineArray[divideLineNumber].trim() === '') {
           divideLineNumber++;
           if (divideLineNumber >= lineArray.length) {
@@ -152,13 +103,15 @@ const getTextArrayFromText = (text) => {
           }
         }
 
-        //here we got a new marker point.
+        // here we got a new marker point.
         marker = divideLineNumber;
         i = divideLineNumber - 1;
         dataArray.push(data);
       }
     }
-    return dataArray;
+    return dataArray.sort((a, b) => {
+      return a.startTimeInMilliSeconds - b.startTimeInMilliSeconds;
+    });
   } catch (e) {
     console.error('getTextArrayFromText', '解析文件出错，文件格式可能不正确。', e.toString());
     return [];
@@ -192,6 +145,7 @@ const getSubtitleTextArrayByUrl = async (url) => {
 
 export {
   getMilliSecondsFromString,
+  getPureText,
   getTextArrayFromText,
   getSubtitleTextArrayByUrl,
 };
